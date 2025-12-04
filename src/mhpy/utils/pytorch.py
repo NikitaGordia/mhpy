632868d47f3543bae5b137a09f2ea5ed40ae8dca
Mykyta Hordia <nikita.gordia@gmail.com>
2025-12-04 14:59:14 +0200

    get_dtype, refactor optim module

--- src/mhpy/utils/pytorch.py
+++ src/mhpy/utils/pytorch.py
@@ -1,5 +1,23 @@
 from loguru import logger
+import torch
 import torch.nn as nn
+
+
+def get_dtype(use_gpu: bool = True, use_bf16: bool = True) -> torch.dtype:
+    if use_gpu and torch.cuda.is_available():
+        support_native_bf16 = torch.cuda.is_bf16_supported(including_emulation=False)
+        use_bf16 = use_bf16 and support_native_bf16
+
+        if use_bf16:
+            return torch.bfloat16
+
+        if not support_native_bf16:
+            logger.warning("bfloat16 is not natively supported. Falling back to float16.")
+        logger.warning("Make sure you use GradScaler for mixed precision!")
+
+        return torch.float16
+
+    return torch.float32
 
 
 def log_model_size(model: nn.Module) -> None:
